e915580617b0f4045b3f0a04ea4f1c9c186ea1d7
s3afeen <zaidsafeen30@gmail.com>
2024-06-23 00:32:45 +0300

    js-2

---
+++ task2.js
@@ -0,0 +1,96 @@
+//-----------1 
+// for(let x = 1 ; x < 11 ; x++){
+//     console.log(x)
+
+
+
+//-----------2 
+// let x = 1;
+// while(x<11)
+//     {
+//         console.log(x);
+//         x++;
+//     }
+
+
+
+//-----------3 
+// for(let z = 0; z < 11;  z=z+2){
+// console.log(z);
+// }
+
+
+
+//-----------4 
+// for(let z = 5 ; z < 6 ; z++){
+//     console.log(z*11);
+// }
+
+
+
+//-----------5 
+// for(let z = 1 ; z < 10 ; z=z+2){
+//     console.log(z);
+// }
+
+
+
+//-----------6 
+// let age= prompt("Please enter your age:");
+// if(age > 16)
+// {
+// document.write('You are an adult! welcom');
+// }
+// else
+// {
+// document.write('You are a young! sorry');
+// }
+
+
+
+//-----------7 
+// let score= prompt("Please enter your score :");
+// if(score > 50)
+// {
+// document.write('You passed the test! welcom');
+// }
+// else
+// {
+// document.write('You failed the test! sorry');
+// }
+
+
+
+//-----------8 
+// let name= prompt("Please enter your name :");
+// document.write(' welcom ' + name);
+
+
+
+//-----------9 
+// let day = prompt("Please enter a day name :");
+// let Saturday = 2;
+// let Friday = 1;
+
+// if (Friday,Saturday)
+//     {
+//         document.write("its a weekend");
+//     }
+//     else
+//     {
+//         document.write("its a weekdaay");
+//     }
+
+
+
+// -----------10 
+// let num = prompt("Please enter a num :");
+
+// if (num % 2 == 0)
+// {
+// document.write('The number is even');
+// }
+// else
+// {
+//     document.write('The number is odd');
+// }
